8591e6d79030a2393c1e8188c5f434a02b35ce6c
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2021-02-01 19:58:57 -0600

    hook custom para fetch

--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustonHook } from './components/01-useState/CounterWithCustonHook';
-import { Simpleform } from './components/02-useEffect/Simpleform';
+// import { Simpleform } from './components/02-useEffect/Simpleform';
 // import { HookApp } from './HookApp';
 
 
 ReactDOM.render(
-  <Simpleform />,
+  <MultipleCustomHooks />,
   document.getElementById('root')
 );
