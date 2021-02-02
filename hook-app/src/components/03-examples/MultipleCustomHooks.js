1fd22bbcddd9d2998ccea70741a219b3280b5dee
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2021-02-01 22:56:30 -0600

    hook custom useFetch

--- hook-app/src/components/03-examples/MultipleCustomHooks.js
+++ hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { useCounter } from '../../hooks/useCounter';
 import { useFetch } from '../../hooks/useFetch'
 import '../02-useEffect/effects.css'
 
 export const MultipleCustomHooks = () => {
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+    const { counter, increment } = useCounter(1)
+    const {loading, data} = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`);
+    // console.log(data);
+    
+    const {author, quote} = !!data && data[0];
+    // console.log(author, quote)
+    
     return (
         <div>
-            <h1>Custom hooks</h1>
+            <h1>Breaking bad</h1>
+            <hr/>
+            {
+                loading 
+                ?
+                    (
+                        <div className="alert alert-finfo text-center">
+                            Loading...
+                        </div>
+                    )
+                :
+                    (
+                        <blockquote className="blockquote text-right">
+                            <p className="mb-0">{quote}</p>
+                            <footer className="blockquote-footer">{author}</footer>
+                        </blockquote>
+                    )
+            }
+            <button 
+                onClick={ increment }
+                className="btn btn-primary"
+            >
+                Siguiente frase
+            </button>
+            
+            
         </div>
     )
 }
--- hook-app/src/hooks/useFetch.js
+++ hook-app/src/hooks/useFetch.js
@@ -8,6 +8,11 @@ export const useFetch = (url) => {
     });
 
     useEffect( ()=>{
+        setState({
+            data:null,
+            loading: true,
+            error:null
+        })
         fetch( url )
             .then( resp => resp.json() )
             .then( data => {
